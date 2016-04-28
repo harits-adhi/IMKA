de2d695ba272752d99261254dac6184ac958fab2
harits-adhi <harits.adhi.p@gmail.com>
2016-04-28 16:36:42 +0700

    Updated tesHC
    We are able to transfer data to local server now! Yahoo!

--- tesHC.ino
+++ tesHC.ino
@@ -9,6 +9,7 @@ void setup() {
   Serial.begin(115200);
   ser.begin(115200);
   connectWifi();
+  sendDataID("10");
 }
 
 void loop() {
@@ -37,6 +38,28 @@ String sendCommand(String command, const int timeout, boolean debug) {
   return response;
 }
 
+void sendDataID(String id) {
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+  
+  cmd += "192.168.0.125";
+  cmd += "\",80\r\n";
+  sendCommand(cmd,1000,DEBUG);
+  delay(5000);
+  
+  String cmd2 = "GET /IMKA/server.php?apples="; // Link ke skrip PHP                    
+  cmd2 += id;  
+  cmd2 += " HTTP/1.1\r\n";
+  cmd2 += "Host: 192.168.0.125\r\n\r\n\r\n"; // Host
+  String pjg="AT+CIPSEND=";
+  pjg += cmd2.length();
+  pjg += "\r\n";
+    
+  sendCommand(pjg,1000,DEBUG);
+  delay(500);
+  sendCommand(cmd2,1000,DEBUG);
+  delay(5000);
+}
+
 void connectWifi() {
   //Set-wifi
   Serial.print("Restart Module...\n");
@@ -51,20 +74,7 @@ void connectWifi() {
   Serial.print("Check IP Address...\n");
   sendCommand("AT+CIFSR\r\n",1000,DEBUG); // get ip address
   delay(5000);
-  Serial.print("Ini apa...\n");
+  /*Serial.print("Ini apa...\n");
   sendCommand("AT+CIPMUX=1\r\n",2000,DEBUG); // reset module
-  delay(5000);
-  Serial.print("Configuring...\n");
-  sendCommand("AT+CIPSTART=4,\"TCP\",\"184.106.153.149\",80\r\n",1000,DEBUG); // get ip address
-  delay(5000);
-  String cmd = "GET https://api.thingspeak.com/update?api_key=1TIP1A4WH38U3VXU&field1=5;";
-  String pjg = "AT+CIPSEND=";
-  pjg += cmd.length();
-  pjg += "\r\n";
-  Serial.print("Configuring...\n");
-  sendCommand(pjg,1000,DEBUG); // get ip address
-  delay(5000);
-  Serial.print("Updating Channel...\n");
-  sendCommand(cmd,3000,DEBUG);
-  delay(5000);
+  delay(5000);*/
 }
